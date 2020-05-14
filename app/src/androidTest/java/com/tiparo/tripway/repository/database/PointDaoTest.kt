47812aff18695c8c1861436a7673378c4ab22c70
vladimirsukiasyan <vova_1999-99@mail.ru>
2020-05-15 01:51:19 +0300

    Added tests on DAOs

---
+++ app/src/androidTest/java/com/tiparo/tripway/repository/database/PointDaoTest.kt
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tiparo.tripway.repository.database
+
+import android.net.Uri
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.android.gms.maps.model.LatLng
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.tiparo.tripway.MainCoroutineRule
+import com.tiparo.tripway.models.Point
+import com.tiparo.tripway.models.Trip
+import com.tiparo.tripway.models.TripWithPoints
+import com.tiparo.tripway.repository.network.api.services.ReverseGeocodingResponse
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.InputStreamReader
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+class PointDaoTest : DbTest() {
+    // Set the main coroutines dispatcher for unit testing.
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun createPoint() = runBlockingTest {
+        // ARRANGE
+        val position = LatLng(-33.8523341, 151.2106085)
+
+        val inputStream =
+            javaClass.classLoader!!.getResourceAsStream("api-response/address_components_locality.json")
+        val addressComponents = Gson().fromJson(
+            InputStreamReader(inputStream, Charsets.UTF_8),
+            object :
+                TypeToken<List<ReverseGeocodingResponse.GeocodingResult.AddressComponent>>() {}.type
+        ) as List<ReverseGeocodingResponse.GeocodingResult.AddressComponent>
+
+        val pickedPhotosOnAdding = listOf(
+            Uri.parse("file:///storage/emulated/0/Android/data/com.tiparo.tripway/files/Pictures/1"),
+            Uri.parse("file:///storage/emulated/0/Android/data/com.tiparo.tripway/files/Pictures/2"),
+            Uri.parse("file:///storage/emulated/0/Android/data/com.tiparo.tripway/files/Pictures/3")
+        )
+
+        val pointOnAdding = Point(
+            location = Point.Location(position, "Salsburg", addressComponents),
+            description = "The best trip ever",
+            photos = pickedPhotosOnAdding,
+            tripId = 1
+        )
+
+        //ACT
+        val pointIdInserted = db.pointDao().insertPoint(pointOnAdding)
+        pointOnAdding.id = pointIdInserted
+
+        //ASSERT
+        val point = db.pointDao().getPointById(pointIdInserted)
+        assertThat(point, `is`(pointOnAdding))
+    }
+
+    @Test
+    fun getTripWithPoints() = runBlockingTest {
+        // ARRANGE
+        val trip =
+            Trip(tripName = "Best ever trip!", isCompleted = false, firstPointName = "Salsburg")
+        val tripIdInserted = db.tripDao().insertTrip(trip)
+        trip.id = tripIdInserted
+
+        val pointsOnAdding = arrayListOf(
+            Point(tripId = tripIdInserted),
+            Point(tripId = tripIdInserted)
+        )
+        pointsOnAdding.forEach {
+            it.id = db.pointDao().insertPoint(it)
+        }
+
+        //ACT
+        val tripWithPointsResult = db.tripDao().getTripWithPoints(tripIdInserted)
+
+        //ASSERT
+        val tripWithPoints  = TripWithPoints(trip, pointsOnAdding)
+        assertThat(tripWithPointsResult, `is`(tripWithPoints))
+    }
+}
---
+++ app/src/androidTest/java/com/tiparo/tripway/repository/database/TripsDaoTest.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tiparo.tripway.repository.database
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.tiparo.tripway.MainCoroutineRule
+import com.tiparo.tripway.models.Trip
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+class TripsDaoTest: DbTest() {
+    // Set the main coroutines dispatcher for unit testing.
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun createTripAndGetById() = runBlockingTest {
+        // GIVEN - insert a trip
+        val trip = Trip(tripName = "Путешествие")
+
+        val id = db.tripDao().insertTrip(trip)
+        // WHEN - Get the trip by id from the database
+        val loaded = db.tripDao().getTripById(id)
+
+        // THEN - The loaded data contains the expected values
+        assertThat<Trip>(loaded, notNullValue())
+        assertThat(loaded.tripName, `is`(trip.tripName))
+    }
+}
