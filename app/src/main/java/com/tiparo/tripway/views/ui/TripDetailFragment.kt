b979dc82fe730691a682a6c07336d29aee0bc063
vladimirsukiasyan <vova_1999-99@mail.ru>
2020-06-01 02:36:33 +0300

    CustomView - StepProgressBar is added

--- app/src/main/java/com/tiparo/tripway/views/ui/TripDetailFragment.kt
+++ app/src/main/java/com/tiparo/tripway/views/ui/TripDetailFragment.kt
@@ -2,15 +2,14 @@ package com.tiparo.tripway.views.ui
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.observe
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -50,6 +49,8 @@ class TripDetailFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+
         viewModel.loadTripWithPoints(args.tripId)
     }
 
@@ -75,11 +76,29 @@ class TripDetailFragment : Fragment() {
         setupViewPager()
         setupMapView()
         setupSnackbar()
+        setupNavigation()
+        setupStepProgressBar()
     }
 
-    private fun setupTitle() {
-        viewModel.tripRoute.observe(viewLifecycleOwner){
-            requireActivity().toolbar.title = it
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.trip_detail_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.deleteTrip -> {
+                viewModel.deleteTrip()
+                true
+            }
+            R.id.deletePoint -> {
+                val pointPosition = binding.pointPager.currentItem
+                viewModel.deletePoint(pointPosition)
+                true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
         }
     }
 
@@ -87,6 +106,21 @@ class TripDetailFragment : Fragment() {
         super.onAttach(context)
 
         (requireActivity().applicationContext as BaseApplication).appComponent.inject(this)
+    }
+
+    private fun setupStepProgressBar() {
+        viewModel.pointsList.observe(viewLifecycleOwner) {
+            binding.stepProgressBar.setStepCount(it.size)
+            binding.stepProgressBar.setOnStepChangedListener { stepIndex->
+                binding.pointPager.currentItem = stepIndex
+            }
+        }
+    }
+
+    private fun setupTitle() {
+        viewModel.tripRoute.observe(viewLifecycleOwner) {
+            requireActivity().toolbar.title = it
+        }
     }
 
     private fun setupMapView() {
@@ -112,14 +146,16 @@ class TripDetailFragment : Fragment() {
     }
 
     private fun setupViewPager() {
-        viewModel.pointsList.observe(viewLifecycleOwner){
+        viewModel.pointsList.observe(viewLifecycleOwner) {
             binding.pointPager.adapter = TripDetailPointPagerAdapter(this, it.size)
-            binding.pointPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            binding.pointPager.registerOnPageChangeCallback(object :
+                ViewPager2.OnPageChangeCallback() {
                 override fun onPageSelected(position: Int) {
                     super.onPageSelected(position)
-                    //TODO изменять позицию камеры
+                    //TODO сохранять текущую позицию viewpager для savedInstanceState
                     val point = viewModel.pointsList.value!![position]
                     moveCamera(point.location.position)
+                    binding.stepProgressBar.updateStep(position)
 
                     Timber.d("ViewPager: position = $position")
                 }
@@ -129,6 +165,12 @@ class TripDetailFragment : Fragment() {
 
     private fun setupSnackbar() {
         view?.setupSnackbar(this, viewModel.snackbarText, Snackbar.LENGTH_LONG)
+    }
+
+    private fun setupNavigation() {
+        viewModel.deletedEvent.observe(viewLifecycleOwner) {
+            findNavController().popBackStack()
+        }
     }
 
     private fun moveCamera(newPosition: LatLng, zoom: Float = DEFAULT_ZOOM) {
